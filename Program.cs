834c83adb3cbc00e09923375069c32126ab3bc43
Alirezamohammadi-13 <149057897+Alirezamohammadi-13@users.noreply.github.com>
2023-11-02 16:55:18 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Alireza mohammadi");
+Console.WriteLine("donbale mosalasi");
+
+
+Console.WriteLine(" donbale mosalasi adad chand?");
+int j = Convert.ToInt32(Console.ReadLine());
+int v = 0;
+for (int m = 1; m <= j; m++)
+{
+    v = v + m;
+
+}
+Console.WriteLine(v);
+Console.WriteLine("tamam");
